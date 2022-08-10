dd99f444e78a6dbe75279d5e9bcae9c32870abe4
Sreenidhi Bendre <67953001+sreenidhi3@users.noreply.github.com>
2022-08-10 17:39:54 +0530

    Delete src directory

--- src/services/users.services.tsx
+++
@@ -1,5 +0,0 @@
-import { User } from "../types/users.types"
-
-export const fetchUsers =():Promise<User[]>=>{ 
-    return fetch("https://api.github.com/users").then(res=>res.json()).then(data=>data as User[])
-}
